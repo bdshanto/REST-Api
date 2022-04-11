349c75a6c9cb41775eef72ad908ec5c530b068e9
bdshanto <bdshanto@hotmail.com>
2022-04-12 01:45:14 +0600

    Implementing versioning

---
+++ TweetBook/Controllers/v1/PostsController.cs
@@ -0,0 +1,22 @@
+﻿using TweetBook.Contracts.v1;
+using TweetBook.Domain;
+
+namespace TweetBook.Controllers.v1;
+
+public class PostsController : Controller
+{
+    private ICollection<Post> _posts;
+    public PostsController()
+    {
+        _posts = new List<Post>();
+        for (var i = 1; i <= 5; i++)
+        {
+                _posts.Add(new Post(){Id = Guid.NewGuid(), Name = $"Name_{i}"});
+        }
+    }
+    [HttpGet(ApiRoutes.Posts.GetAll)]
+    public IActionResult GetAll()
+    {
+        return Ok(_posts);
+    }
+}
---
+++ TweetBook/Domain/Post.cs
@@ -0,0 +1,7 @@
+﻿namespace TweetBook.Domain;
+
+public class Post
+{
+    public Guid Id { get; set; }
+    public string Name { get; set; }
+}
